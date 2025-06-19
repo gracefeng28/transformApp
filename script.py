54b0b991642f17854795d780bdbff6f2b6bc2776
Grace Feng <fengrace@umich.edu>
2025-06-19 09:41:22 -0400

    fixed bug

--- script.py
+++ script.py
@@ -2,19 +2,21 @@
 #import sample file
 import pandas as pd
 import numpy as np
-from boxcox import run_boxcox 
 import numpy as np
 import csv
 
 
 
 df = pd.read_csv("GWAS_Ionomics_Phenotype.txt", delimiter= "\t")
-#print(df.loc[:,"Mo98"])
+print(df.loc[:,"Mo98"])
+print(df.columns)
 cols = list((df.columns))[1:]
-print(cols)
+
+#print(cols)
 
 def run_boxcox(trait_list):
-    print(df.loc[:trait_list])
+    data = (list(df.loc[:,"B11"]))
+    print(type(data))
 
 output_tsv = 'box_cox.tsv'
 tsv_header_name = ['id', 'firstname', 'lastname', 'age']
@@ -23,18 +25,17 @@ each_row = [
 ]
 #with open(output_tsv, 'w') as tsvfile:
     #csv writer to write in tsv file
-    tsv_writer = csv.writer(tsvfile, delimiter='\t')
+    #tsv_writer = csv.writer(tsvfile, delimiter='\t')
     #write header in tsv file
-    tsv_writer.writerow(tsv_header_name)
+    #tsv_writer.writerow(tsv_header_name)
     #write rows
-    tsv_writer.writerows(each_row)
+    #tsv_writer.writerows(each_row)
     #close csv file
-    tsvfile.close()
-    pass
-
-
+    #tsvfile.close()
+    #pass
 
 
 for col in cols:
+    print(col)
     run_boxcox(col)
     
