d63f59a72887428a61a3b53ff7707ea989daac7a
Grace Feng <fengrace@umich.edu>
2025-06-19 09:30:40 -0400

    edits

--- script.py
+++ script.py
@@ -1,10 +1,40 @@
 
 #import sample file
 import pandas as pd
+import numpy as np
+from boxcox import run_boxcox 
+import numpy as np
+import csv
+
+
+
 df = pd.read_csv("GWAS_Ionomics_Phenotype.txt", delimiter= "\t")
 #print(df.loc[:,"Mo98"])
 cols = list((df.columns))[1:]
 print(cols)
 
+def run_boxcox(trait_list):
+    print(df.loc[:trait_list])
+
+output_tsv = 'box_cox.tsv'
+tsv_header_name = ['id', 'firstname', 'lastname', 'age']
+each_row = [
+    ['1', 'James', 'Moore', '10'],
+]
+#with open(output_tsv, 'w') as tsvfile:
+    #csv writer to write in tsv file
+    tsv_writer = csv.writer(tsvfile, delimiter='\t')
+    #write header in tsv file
+    tsv_writer.writerow(tsv_header_name)
+    #write rows
+    tsv_writer.writerows(each_row)
+    #close csv file
+    tsvfile.close()
+    pass
+
+
+
+
 for col in cols:
-    output = run_boxcox(col)
+    run_boxcox(col)
+    
